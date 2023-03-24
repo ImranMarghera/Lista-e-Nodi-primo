bc4d01bd3897f35083519eb81e2055382d575ec1
Mohammad Imran <mohammad@Collalto.local>
2023-03-24 13:48:21 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Nodo n1 = new Nodo(2);
+        Nodo n2 = new Nodo(14);
+
+        Lista l1 = new Lista();
+
+        l1.start= n1;
+        n1.next = null;
+
+
+
+
+
+
+
+    }
+}
---
+++ src/Nodo.java
@@ -0,0 +1,13 @@
+public class Nodo {
+
+    public int valore;
+    public Nodo next;
+    public Nodo(int valore)
+    {
+        this.valore = valore;
+        next = null;
+    }
+
+
+
+}
